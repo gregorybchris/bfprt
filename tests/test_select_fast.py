7030fe27585054e171866bcbdf97dbc58339cdfb
Chris Gregory <gregorybchris@gmail.com>
2024-09-17 11:08:22 -0700

    Add test for comparable types

--- tests/test_select_fast.py
+++ tests/test_select_fast.py
@@ -17,3 +17,17 @@ class TestSelectFast:
             selected_index = select_fast(items, k)
             sorted_items = [1, 2, 4, 5, 8, 9]
             assert items[selected_index] == sorted_items[k]
+
+    def test_comparable(self) -> None:
+        class Item:
+            def __init__(self, value: int) -> None:
+                self.value = value
+
+            def __lt__(self, other: "Item") -> bool:
+                return self.value < other.value
+
+        items = [Item(4), Item(2), Item(1), Item(9), Item(5), Item(8)]
+        k = 3
+        selected_index = select_fast(items, k)
+        sorted_items = [Item(1), Item(2), Item(4), Item(5), Item(8), Item(9)]
+        assert items[selected_index].value == sorted_items[k].value
